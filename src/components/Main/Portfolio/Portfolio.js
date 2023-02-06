bba37980ceaf0ba87ab5739e39ccd3c74ccf84ae
yessena-21 <bambr78@yandex.ru>
2023-02-06 15:51:02 +0300

    исправлен список. и lang

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -8,20 +8,20 @@ function Portfolio() {
                 <h2 className='portfolio__subtitle'>
                     Портфолио
                 </h2>
-                <li className='portfolio__links'>
-                    <ul className='portfolio__link'>
+                <ul className='portfolio__links'>
+                    <li className='portfolio__link'>
                         <a href='https://github.com/yessena-21/how-to-learn' target='_blank' className='portfolio__link-text' rel="noreferrer">Статичный сайт</a>
                         <a href='https://github.com/yessena-21/how-to-learn' target='_blank' className='portfolio__link-text' rel="noreferrer">↗</a>
-                    </ul>
-                    <ul className='portfolio__link'>
+                    </li>
+                    <li className='portfolio__link'>
                         <a href='https://yessena-21.github.io/russian-travel/' target='_blank' className='portfolio__link-text' rel="noreferrer">Адаптивный сайт</a>
                         <a href='https://yessena-21.github.io/russian-travel/' target='_blank' className='portfolio__link-text' rel="noreferrer">↗</a>
-                    </ul>
-                    <ul className='portfolio__link'>
+                    </li>
+                    <li className='portfolio__link'>
                         <a href='https://github.com/yessena-21/react-mesto-api-full' target='_blank' className='portfolio__link-text' rel="noreferrer">Одностраничное приложение</a>
                         <a href='https://github.com/yessena-21/react-mesto-api-full' target='_blank' className='portfolio__link-text' rel="noreferrer">↗</a>
-                    </ul>
-                </li>
+                    </li>
+                </ul>
             </div>
         </section>
     )
